2535b12973eb67106491ffb4054c2e92570f68aa
SanathoiAngom <117849539+SanathoiAngom@users.noreply.github.com>
2025-09-04 19:38:09 +0530

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,8 @@
+#Task 2 : Using loop to iterate over numbers from 1 to 50.
+
+total_sum = 0
+
+for num in range(1, 51):    #using i for number
+    total_sum += num
+    print("The sum of numbers from 1 to 50 is: ", total_sum)
+
